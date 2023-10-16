afdbec9c9abc314d87fc6aeea1d139f18f8839d8
AnyySann <86086338+anyy520@users.noreply.github.com>
2023-10-16 00:54:01 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var trex ,trex_running;
+
+
+//carregar animações
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //crie um sprite de trex
+
+
+  //adicione dimensão ao trex
+
+
+  //crie um sprite ground (solo)
+
+ 
+}
+
+function draw(){
+  background("white")
+
+  //pular quando a tecla espaço for pressionada
+
+  
+   //simula a gravidade para "cair" após clicar a tecla de pular
+
+  //impedir que o trex caia (por conta da gravidade)
+
+}
